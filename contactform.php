0863e07962ff10ccf5c4c80a4cc2d38ce50ecf17
RickBerends <48214129+RickBerends@users.noreply.github.com>
2019-07-07 15:55:14 +0200

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -3,10 +3,10 @@
 if (isset($_POST['submit'])) {
 $name = $_POST['name'];
 $subject = $_POST['subject'];
-$mailTo = $_POST['mail'];
+$mailFrom = $_POST['mail'];
 $message = $_POST['message'];
 
-$mailFrom = "rickberends10@gmail.com";
+$mailTo = "rickberends10@gmail.com";
 $headers = "From: keep4students";
 $txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
